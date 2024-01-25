bfeb07e58c4dd528b0ad65f97fe49ee8b42a706c
somros <alberto.rovellini@gmail.com>
2024-01-24 17:29:48 -0800

    set up input files for MS runs

--- RunAtlantis_96.sh
+++
@@ -1,1 +0,0 @@
-atlantisMerged -i GOA_cb_summer.nc  0 -o output_96.nc -r GOA_run.prm -f GOA_force.prm -p GOA_physics.prm -b GOAbioparam_test.prm -h GOA_harvest_96.prm -m GOAMigrations.csv -s GOA_Groups.csv -q GOA_fisheries.csv -d output
